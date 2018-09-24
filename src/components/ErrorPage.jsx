16328554f28f7667709ebf7820f860edd9275c52
willensoll <william.ensoll@gmail.com>
2018-09-24 08:57:11 +0100

    bug fix

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div style={{ "margin-left": "33%" }}>
+        <div style={{ marginLeft: "33%" }}>
             <h1>ah ah ah, you didnt say the magic word!</h1>
-            <img style={{ "margin-left": "10%" }} src="/errorgif.gif" alt="error gif!" />
+            <img style={{ marginLeft: "10%" }} src="/errorgif.gif" alt="error gif!" />
             <h4>Sorry! An error has occured, please click <Link to={'/'}>here</Link> to go back to the mainpage</h4>
         </div>
 
