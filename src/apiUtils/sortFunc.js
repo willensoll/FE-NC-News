67f1717611473caa32809613ef8afeb8ed461d03
willensoll <william.ensoll@gmail.com>
2018-10-07 18:04:45 +0100

    reimplemented sort and add comment function added loading bars instead of text

---
+++ src/apiUtils/sortFunc.js
@@ -0,0 +1,24 @@
+
+const sort = (array, type) => {
+    console.log('function called')
+     switch (type) {
+        case 'new':
+        array.sort((a, b) => b.created_at.localeCompare(a.created_at))
+        break;
+        case 'old':
+        array.sort((a, b) => a.created_at.localeCompare(b.created_at))
+        break;
+        case 'hot':
+        array.sort((a, b) => b.votes - a.votes)
+        break;
+        case 'not': 
+        array.sort((a, b) => a.votes - b.votes)
+        break;
+        default: 
+        array.sort((a, b) => b.created_at.localeCompare(a.created_at))
+    }
+    return array
+}
+
+
+export default sort;
